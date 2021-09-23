8a4b38fbe2c354fc845db7769a7cbf32055fe6b7
nicolasso <nicolassocac@gmmail.com>
2021-09-23 01:44:29 -0300

    cambios en archivo app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("hola ameoooo")
+// Requisitos para viajar al extranjero
+
+const edad = Number(prompt("Ingrese su edad"));
+const vacunas = Number(prompt("Cuantas dosis de vacuna tiene aplicadas?"));
+
+const destino = confirm("Quiere viajar dentro del país?");
+/* true => nacional
+   false => extranjero */
+   
+const aprobacion = (!destino && edad >=18 && vacunas >=1) || (destino);
+
+if (aprobacion) {
+    alert("Su viaje ha sido autorizado")
+} else {
+    alert ("No cumple los requisitos para viajar al extranejero")
+}
